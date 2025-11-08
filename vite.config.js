f6dcd5b2d3150df3b4a4787ba01166b33269d70a
vipzjhvip <124351637+vipzjhvip@users.noreply.github.com>
2025-11-08 12:13:33 +0800

    Configure project root and build output directory
    Set project root and output directory for build.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'), // ✅ 指定项目源代码根目录
   plugins: [react()],
-  base: './',
+  base: './', // 保证构建资源使用相对路径
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // 输出目录为 dist
+    emptyOutDir: true,
+  }
 })
